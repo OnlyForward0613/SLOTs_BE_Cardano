f9fb1344f3090cdcc09ca07657c6e6f5e8d91b3d
OnlyForward0613 <harryborison613@gmail.com>
2023-07-16 17:39:52 +0900

    add backend code

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const config = {
+  BLOCKFROST_API_URL: "https://cardano-preprod.blockfrost.io/api/v0/",
+  // BLOCKFROST_API_URL: "https://cardano-mainnet.blockfrost.io/api/v0/",
+  BLOCKFROST_API_KEY: "preprodprM8Q5AdIfMRSRI2FhcIrAHVFx7X0XI4",
+  // BLOCKFROST_API_KEY: "mainnetBarCr4bVgOBTWDFhRsmwc3VNdpSlSARz",
+  CARDANO_NETWORK: 0,
+  // CARDANO_NETWORK: 1,
+  PREVIEW_OR_PREPROD: "Preprod",
+  CARDANOSCAN_URL: "https://preprod.cardanoscan.io/",
+  // CARDANOSCAN_URL: "https://explorer.cardano.org/en",
+}
